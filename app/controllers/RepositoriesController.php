a59a7b86868269ab6bb2e06e458eb8ffe4b9bfe0
Meisam Ghanbari <m.q.php.98@gmail.com>
2022-08-22 02:21:03 +0430

    Complete RepositoriesController

--- app/controllers/RepositoriesController.php
+++ app/controllers/RepositoriesController.php
@@ -17,11 +17,56 @@ use Meygh\GithubApi\Base\Controller;
  */
 class RepositoriesController extends Controller
 {
-    public function actionIndex()
+    public function filterVerbs()
     {
         return [
-            'status' => true,
-            'msg' => 'Repositories Controller Action index!'
+            'index' => 'GET'
         ];
+    }
+
+    public function actionIndex()
+    {
+        // Takes raw data from the request
+        $json = file_get_contents('php://input');
+        $params = json_decode($json);
+
+        $api_result = (array) Service()->GitHubClient->user()->repos();
+        $result = [];
+
+        if (!isset($api_result['message'])) {
+            $last_user = '';
+            $repositories = [];
+
+            foreach ($api_result as $repo) {
+                if ($last_user && $last_user != $repo->owner->login) {
+                    $result[] = [
+                        'username' => $last_user,
+                        'repositories' => $repositories
+                    ];
+
+                    $repositories = [];
+                }
+
+                $repositories[] = [
+                    'full_name' => $repo->full_name,
+                    'url' => $repo->html_url
+                ];
+
+                $last_user = $repo->owner->login;
+            }
+
+            $result[] = [
+                'username' => $last_user,
+                'repositories' => $repositories
+            ];
+        }
+
+        if (isset($params->user)) {
+            $result = array_filter($result, function ($item) use ($params) {
+                return ($item['username'] == $params->user);
+            });
+        }
+
+        return $result;
     }
 }
